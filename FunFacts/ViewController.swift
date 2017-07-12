bb264e4bc831015aef8853bf4319a2cb9462cc32
bnasholm <breannanasholm@gmail.com>
2017-07-12 12:41:33 -0700

    Added title label, color and fixed auto layout so works on different devcies

--- FunFacts/ViewController.swift
+++ FunFacts/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var FunFactLabel: UILabel!
+    let factProvider = FactProvider()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        FunFactLabel.text = factProvider.randomFact()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +23,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func showFact() {
+        FunFactLabel.text = factProvider.randomFact()
+    }
 
 }
 
